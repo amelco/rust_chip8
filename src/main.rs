facfd0a8f3521fa8350e028523b12a864fc4774f
Andre Herman <amelco.herman@gmail.com>
2022-06-04 23:11:53 -0300

    Added cpu

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ use chip8::Chip8;
 
 mod ram;
 mod chip8;
+mod cpu;
 
 fn main() {
     let mut file = File::open("data/INVADERS").unwrap();
     let mut data: Vec<u8> = Vec::new();
-    file.read_to_end(&mut data);
+    file.read_to_end(&mut data).unwrap_or_default();
 
     let mut chip8 = Chip8::new();
     chip8.load_rom(&data);
+
+    loop {
+        chip8.run_instruction();
+    }
 
 }
